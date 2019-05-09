f2f9aba4d020e929b8512f96bbdbb034afd0d166
Victor Pedrazzi <vspedrazzi@gmail.com>
2019-05-08 22:31:38 -0300

    Part 2: Navigate to a new screen

--- lib/main.dart
+++ lib/main.dart
@@ -17,9 +17,40 @@ class RandomWordsState extends State<RandomWords> {
   final List<WordPair> _suggestions = <WordPair>[];
   final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
   final Set<WordPair> _saved = Set<WordPair>();
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        builder: (BuildContext context) {
+          final Iterable<ListTile> tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            }
+          );
+          final List<Widget> divided = ListTile
+            .divideTiles(
+              context: context,
+              tiles: tiles,
+            )
+            .toList();
+          
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        },
+      ),
+    );
+  }
 
   Widget _buildSuggestions() {
-  return ListView.builder(
+    return ListView.builder(
       padding: const EdgeInsets.all(16.0),
       itemBuilder: /*1*/ (context, i) {
         if (i.isOdd) return Divider(); /*2*/
@@ -29,10 +60,11 @@ class RandomWordsState extends State<RandomWords> {
           _suggestions.addAll(generateWordPairs().take(10)); /*4*/
         }
         return _buildRow(_suggestions[index]);
-      });
+      }
+    );
   }
 
-    Widget _buildRow(WordPair pair) {
+  Widget _buildRow(WordPair pair) {
     final bool alreadySaved = _saved.contains(pair); 
     return ListTile(
       title: Text(
@@ -60,6 +92,9 @@ class RandomWordsState extends State<RandomWords> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Startup Name Generator'),
+        actions: <Widget>[
+          IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+        ],
       ),
       body: _buildSuggestions(),
     );
